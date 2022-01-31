61b24833580c1ed8f1fdc54d6fe96028ff6248a9
zhiyi98 <742801893@qq.com>
2022-01-31 15:31:59 +0800

    update

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-console.log("helloworld")
-console.log("helloworld")
-console.log("helloworld")
-console.log("helloworld")
+console.log("helloworld");
+console.log("helloworld");
+console.log("helloworld");
+console.log("helloworld");
+
+const fs = require("fs");
+
+fs.writeFile("test.txt", "I love Shanghai!\n", { "flag": "a+" }, (err) => {
+  if (err) console.log(err);
+  console.log("写入成功！");
+});
+
+console.log("运行结束~");
