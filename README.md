df41cb6a1ea7bac7c6d481707b34bfe8da068547
zhiyi98 <742801893@qq.com>
2022-02-01 19:14:53 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-1/31/2022
-![](https://cn.bing.com/th?id=OHR.ChineseNewYearEve_ZH-CN1901922324_1920x1080.jpg&rf=LaDigue_1920x1080.jpg)
+## Bing Wallpaper
+
+![](https://cn.bing.com/th?id=OHR.ChineseNewYear_ZH-CN5844255626_1920x1080.jpg&rf=LaDigue_1920x1080.jpg)1/2/2022
+
+| | | |
+| - | - | - |
+|![](https://cn.bing.com/th?id=OHR.ChineseNewYear_ZH-CN5844255626_1920x1080.jpg&rf=LaDigue_1920x1080.jpg)2/1/2022|![](https://cn.bing.com/th?id=OHR.ChineseNewYearEve_ZH-CN5742088045_1920x1080.jpg&rf=LaDigue_1920x1080.jpg)1/31/2022|
--- index.js
+++ index.js
@@ -1,8 +1,4 @@
-console.log("helloworld");
-console.log("helloworld");
-console.log("helloworld");
-console.log("helloworld");
-
+console.log("Wellcome~");
 // const fs = require("fs");
 
 // fs.writeFile("test.txt", "I love Shanghai!\n", { "flag": "a+" }, (err) => {
@@ -18,18 +14,72 @@ const fs = require('fs');
 async function start() {
   const browser = await puppeteer.launch(/*{ headless: false }*/);
   const page = await browser.newPage();
-  await page.goto("https://cn.bing.com/");  
+  await page.goto("https://cn.bing.com/");
+
+  // 得到img
   await page.waitForSelector("body > div.hpapp > div > div:nth-child(1) > div");
   const img = await page.evaluate(() => {
     return "https://cn.bing.com" + document.querySelector("body > div.hpapp > div > div:nth-child(1) > div").style.backgroundImage.split('"')[1]
   })
   console.log(img)
+
+  // 添加到markdown里的格式
+  const img_data = "![](" + img + ")" + new Date().toLocaleDateString();
   // 写到文件里
-  const data = new Date().toLocaleDateString() + "\n" + "![](" + img + ")\n";
-  await fs.writeFile("README.md", data, { 'flag': "a+" }, (err) => {
-    if (err) console.log("err");
-    console.log('The file has been saved!\t'+img);
+  await fs.readFile("README.md", "utf-8", (err, data) => {
+    if (err) throw err;
+    let res = getData(data);
+    let r = addImg(res, img_data);
+    fs.writeFile("README.md", r, (err) => {
+      if (err) throw err;
+      console.log("done!");
+    });
   });
   await browser.close();
+
 };
 start();
+
+
+
+
+
+
+// 得到所有img数据
+function getData(data) {
+  let res = [];
+  data.split("\n").splice(6).forEach(x => {
+    res += x;
+  });
+  res = res.split("|");
+  for (let i = 0; i < res.length; i++) {
+    if (res[i] === "") {
+      res.splice(i, 1);
+    };
+  };
+  return res;
+};
+
+// 添加img和一些标题、格式
+function addImg(res, img) {
+  res.unshift(img);
+  let r = "";
+  r += "## Bing Wallpaper\n\n";
+  r += img;
+  r += "\n\n| | | |\n";
+  r += "| - | - | - |\n";
+
+  for (let i = 0; i < res.length; i++) {
+    if ((i + 1) % 3 === 0) {
+      r += "|" + res[i] + "|\n";
+    } else {
+      r += "|" + res[i];
+    };
+  };
+  if ((res.length) % 3 === 0) {
+    ;
+  } else {
+    r += "|";
+  };
+  return r;
+};
