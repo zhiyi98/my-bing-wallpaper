158274a5f08ad2a2b63f4efc5e0061634e05e84d
zhiyi98 <742801893@qq.com>
2022-02-11 19:54:02 +0800

    update

--- README.md
+++ README.md
@@ -1,11 +1,9 @@
 ## Bing Wallpaper
 
-![](https://cn.bing.com/th?id=OHR.TeaGardensMunnar_ZH-CN9587720369_1920x1080.jpg&rf=LaDigue_1920x1080.jpg)2/11/2022
+![](https://cn.bing.com/th?id=OHR.SnowyBern_ZH-CN5472524801_1920x1080.jpg&rf=LaDigue_1920x1080.jpg)2022/2/10
 
 | | | |
 | - | - | - |
-|![](https://cn.bing.com/th?id=OHR.TeaGardensMunnar_ZH-CN9587720369_1920x1080.jpg&rf=LaDigue_1920x1080.jpg)2/11/2022|![](https://cn.bing.com/th?id=OHR.SnowyBern_ZH-CN5472524801_1920x1080.jpg&rf=LaDigue_1920x1080.jpg)2/10/2022|![](https://cn.bing.com/th?id=OHR.SevenSistersCliffs_ZH-CN5362127173_1920x1080.jpg&rf=LaDigue_1920x1080.jpg)2/9/2022|
-|![](https://cn.bing.com/th?id=OHR.SpeloncatoSnow_ZH-CN8115437163_1920x1080.jpg&rf=LaDigue_1920x1080.jpg)2/8/2022|![](https://cn.bing.com/th?id=OHR.WinterludeIce_ZH-CN7868524911_1920x1080.jpg&rf=LaDigue_1920x1080.jpg)2/7/2022||
-|![](https://cn.bing.com/th?id=OHR.Oymyakon_ZH-CN7758768574_1920x1080.jpg&rf=LaDigue_1920x1080.jpg)2/6/2022|![](https://cn.bing.com/th?id=OHR.MexicoMonarchs_ZH-CN7526758236_1920x1080.jpg&rf=LaDigue_1920x1080.jpg)2/5/2022|![](https://cn.bing.com/th?id=OHR.WinterOlymics_ZH-CN7384614076_1920x1080.jpg&rf=LaDigue_1920x1080.jpg)2/4/2022|
-||![](https://cn.bing.com/th?id=OHR.WinterOlymics_ZH-CN7384614076_1920x1080.jpg&rf=LaDigue_1920x1080.jpg)2/4/2022|![](https://cn.bing.com/th?id=OHR.WinterOlymics_ZH-CN7384614076_1920x1080.jpg&rf=LaDigue_1920x1080.jpg)2/4/2022|
-|![](https://cn.bing.com/th?id=OHR.FortCorjuem_ZH-CN7295613217_1920x1080.jpg&rf=LaDigue_1920x1080.jpg)2/3/2022||
+|![](https://cn.bing.com/th?id=OHR.SnowyBern_ZH-CN5472524801_1920x1080.jpg&rf=LaDigue_1920x1080.jpg)2022/2/10|![](https://cn.bing.com/th?id=OHR.SevenSistersCliffs_ZH-CN5362127173_1920x1080.jpg&rf=LaDigue_1920x1080.jpg)2022/2/9|![](https://cn.bing.com/th?id=OHR.SpeloncatoSnow_ZH-CN8115437163_1920x1080.jpg&rf=LaDigue_1920x1080.jpg)2022/2/8|
+|![](https://cn.bing.com/th?id=OHR.WinterludeIce_ZH-CN7868524911_1920x1080.jpg&rf=LaDigue_1920x1080.jpg)2022/2/7|![](https://cn.bing.com/th?id=OHR.Oymyakon_ZH-CN7758768574_1920x1080.jpg&rf=LaDigue_1920x1080.jpg)2022/2/6|![](https://cn.bing.com/th?id=OHR.MexicoMonarchs_ZH-CN7526758236_1920x1080.jpg&rf=LaDigue_1920x1080.jpg)2022/2/5|
+|![](https://cn.bing.com/th?id=OHR.WinterOlymics_ZH-CN7384614076_1920x1080.jpg&rf=LaDigue_1920x1080.jpg)2022/2/4|
--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-console.log("Wellcome~");
-// const fs = require("fs");
-
-// fs.writeFile("test.txt", "I love Shanghai!\n", { "flag": "a+" }, (err) => {
-//   if (err) console.log(err);
-//   console.log("写入成功！");
-// });
-
-// console.log("运行结束~");
-
 const puppeteer = require('puppeteer');
 const fs = require('fs');
 
@@ -21,65 +11,56 @@ async function start() {
   const img = await page.evaluate(() => {
     return "https://cn.bing.com" + document.querySelector("body > div.hpapp > div > div:nth-child(1) > div").style.backgroundImage.split('"')[1]
   })
-  console.log(img)
+  // console.log(img)
 
   // 添加到markdown里的格式
   const img_data = "![](" + img + ")" + new Date().toLocaleDateString();
   // 写到文件里
-  await fs.readFile("README.md", "utf-8", (err, data) => {
-    if (err) throw err;
-    let res = getData(data);
-    let r = addImg(res, img_data);
-    fs.writeFile("README.md", r, (err) => {
-      if (err) throw err;
-      console.log("done!");
-    });
-  });
+  fs.readFile("./README.md", "utf-8", (err, data) => {
+    if (err) console.log(err);
+
+    data = getData(data);
+    data.unshift(img_data);
+    let content = addImg(data);
+
+    fs.writeFile("README1.md", content, (err) => {
+      if (err) console.log(err)
+      console.log("done!")
+    })
+
+  })
+
   await browser.close();
 
 };
 start();
 
-
-
-
-
-
 // 得到所有img数据
 function getData(data) {
-  let res = [];
-  data.split("\n").splice(6).forEach(x => {
-    res += x;
-  });
-  res = res.split("|");
-  for (let i = 0; i < res.length; i++) {
-    if (res[i] === "") {
-      res.splice(i, 1);
-    };
-  };
-  return res;
+  var regex = /!\[\]\(http[s]{0,1}:\/\/(.*?)\.jpg\)\d{1,4}\/\d{1,4}\/\d{1,4}/g;
+  data = data.match(regex).splice(1);
+  return data;
 };
 
 // 添加img和一些标题、格式
-function addImg(res, img) {
-  res.unshift(img);
-  let r = "";
-  r += "## Bing Wallpaper\n\n";
-  r += img;
-  r += "\n\n| | | |\n";
-  r += "| - | - | - |\n";
-
-  for (let i = 0; i < res.length; i++) {
-    if ((i + 1) % 3 === 0) {
-      r += "|" + res[i] + "|\n";
-    } else {
-      r += "|" + res[i];
-    };
-  };
-  if ((res.length) % 3 === 0) {
-    ;
-  } else {
-    r += "|";
-  };
-  return r;
+function addImg(data) {
+  let content = "";
+  content += "## Bing Wallpaper";
+  content += "\r\n";
+  content += "\r\n";
+  content += data[0];
+  content += "\r\n";
+  content += "\r\n";
+  content += "| | | |";
+  content += "\r\n";
+  content += "| - | - | - |";
+  content += "\r\n";
+  for (let i = 0; i < data.length; i++) {
+    content += "|" + data[i]
+    if ((i + 1) % 3 === 0 || (i + 1) === data.length) {
+      content += "|";
+      content += "\r\n";
+    }
+  }
+  return content;
 };
